cf66b8ddba771689aae70b2acf18a1fdd4be2b59
kimalise <jinguozhe@ybu.edu.cn>
2025-03-28 11:15:54 +0800

    mvsa complete

--- 2025/mvsa_sentiment_analysis.py
+++ 2025/mvsa_sentiment_analysis.py
@@ -3,9 +3,23 @@ import numpy as np
 from torchvision.models import resnet50
 from transformers import BertTokenizer, BertModel
 import os
+from torch.utils.data import Dataset, DataLoader
+import cv2
+import torchvision.transforms as transforms
 
 train_ratio = 0.8
 root = "./data/MVSA_Single"
+epochs = 10
+learning_rate = 1e-3
+batch_size = 4
+
+transforms = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize((224, 224)),
+    transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+])
+
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased", cache_dir="./cache")
 
 def read_data():
     image_files = []
@@ -62,9 +76,117 @@ def split_dataset(examples, train_ratio):
 
     return examples[:num_train], examples[num_train:]
 
-if __name__ == '__main__':
+class MVSADataset(Dataset):
+    def __init__(self, examples, transforms, label_vocab):
+        super(MVSADataset, self).__init__()
+        self.examples = examples
+        self.transforms = transforms
+        self.label_vocab = label_vocab
+
+    def __getitem__(self, index):
+        example = self.examples[index]
+        img = cv2.imread(os.path.join(root, "data", example['image']), cv2.IMREAD_UNCHANGED)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img_tensor = self.transforms(img)
+
+        with open(os.path.join(root, "data", example['text']), "r", encoding="utf-8") as f:
+            text = f.readlines()[0]
+
+        label = self.label_vocab[example["label"]]
+
+        return img_tensor, text, label
+
+    def __len__(self):
+        return len(self.examples)
+
+def collate_fn(batch):
+    batch_text = []
+    batch_img = []
+    batch_label = []
+    for item in batch:
+        batch_img.append(item[0])
+        batch_text.append(item[1])
+        batch_label.append(item[2])
+
+    img_tensor = torch.stack(batch_img, dim=0)
+
+    text_tensor = tokenizer.batch_encode_plus(
+        batch_text_or_text_pairs=batch_text,
+        padding="max_length",
+        max_length=512,
+        truncation=True,
+        return_length=True,
+        return_tensors="pt",
+    )
+    label_tensor = torch.tensor(batch_label, dtype=torch.long)
+
+    return img_tensor, text_tensor, label_tensor
+
+def test_dataset():
     image_files, text_files = read_data()
     label_dict = read_labels()
     examples, label_vocab = generate_exmaples(image_files, text_files, label_dict)
     train_exmaples, val_exmaples = split_dataset(examples, train_ratio)
+
+    dataset = MVSADataset(train_exmaples, transforms, label_vocab)
+    dataloader = DataLoader(dataset, batch_size=4, shuffle=False, collate_fn=collate_fn)
+    for step, batch in enumerate(dataloader):
+        print(batch)
+        break
+
+class MVSAModel(torch.nn.Module):
+    def __init__(self):
+        super(MVSAModel, self).__init__()
+        resnet = resnet50(pretrained=True)
+        self.image_encoder = torch.nn.Sequential(*resnet.children())[:-1]
+        # model = torch.nn.Sequential(A, B, C)
+        self.text_encoder = BertModel.from_pretrained("bert-base-uncased", cache_dir="./cache")
+        self.fc = torch.nn.Linear(2048 + 768, 3)
+
+    def forward(self, img_tensor, text_tensor):
+        img_feature = self.image_encoder(img_tensor) # [B, 3, h, w] -> [B, 2048, 1, 1]
+        img_feature = torch.squeeze(img_feature) # [B, 2048]
+
+        text_feature = self.text_encoder(
+            text_tensor['input_ids'],
+            text_tensor['attention_mask'],
+            text_tensor['token_type_ids']
+        )
+        text_feature = text_feature['pooler_output'] # cls [B, 768]
+        feature = torch.cat([img_feature, text_feature], dim=1) # [B, 2048 + 768]
+        output = self.fc(feature) # [B, 3]
+        return output
+
+def train(model, dataloader):
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    model.train()
+
+    for epoch in range(epochs):
+        for step, batch in enumerate(dataloader):
+            img_tensor = batch[0]
+            text_tensor = batch[1]
+            label_tensor = batch[3]
+
+            pred_logits = model(img_tensor, text_tensor)
+            loss = criterion(pred_logits, label_tensor)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if step % 100 == 0:
+                print("epoch: {}, step: {}, loss: {:.4f}".format(epoch, step, loss.item()))
+
+
+if __name__ == '__main__':
+    # image_files, text_files = read_data()
+    # label_dict = read_labels()
+    # examples, label_vocab = generate_exmaples(image_files, text_files, label_dict)
+    # train_exmaples, val_exmaples = split_dataset(examples, train_ratio)
+    test_dataset()
     print("complete")
+
+
+
+
